fd47725c318c22f8b906c0a892699110ed074a7e
bashiru98 <atulebashirubukari@gmail.com>
2022-04-27 05:03:50 +0300

    solve  field issues in config file

--- setup.sh
+++ setup.sh
@@ -102,15 +102,12 @@ cd $HOME_DIR
 printf '\n\n' | pocket accounts create
 
 #================================Assign pocket permission && sudo group to files in $HOME_DIR/.pocket/======================
-cd $HOME_DIR/ && echo "$pass" | sudo chown -R pocket ~/.pocket/ && echo "$pass" | sudo chgrp -R sudo ~/.pocket/
+cd $HOME_DIR/ && echo "$pass" | sudo chown -R pocket .pocket/ && echo "$pass" | sudo chgrp -R sudo .pocket/
 
 # -- get account and export private key --
 ACCOUNTS=$(pocket accounts list)
 ACCOUNT=$(echo "${ACCOUNTS}" | head -1 | cut -d' ' -f2)
 PRIVATE_KEY=$(printf '\n\n\n' | pocket accounts export --path . $ACCOUNT)
-
-#================================Assign pocket permission && sudo group to files in $HOME_DIR/.pocket/======================
-cd $HOME_DIR/ && echo "$pass" | sudo chown -R pocket ~/.pocket/ && echo "$pass" | sudo chgrp -R sudo ~/.pocket/
 
 # -- set account as validator address --
 printf '\n\n\n' | pocket accounts set-validator $ACCOUNT
